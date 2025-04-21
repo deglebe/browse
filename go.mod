db3acbcb87cf8411ba67b9a39e9d046bfe81f269
deglebe <tdb@tdbio.me>
2025-04-21 01:26:32 -0400

    sized context

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/deglebe/browse
 go 1.23.4
 
 require golang.org/x/image v0.26.0
+
+require golang.org/x/text v0.24.0 // indirect
--- pkg/layout/render.go
+++ pkg/layout/render.go
@@ -5,23 +5,70 @@ import (
 	"strings"
 
 	"github.com/deglebe/browse/pkg/dom"
-
 	"golang.org/x/image/font"
+	"golang.org/x/image/font/gofont/goregular"
+	"golang.org/x/image/font/opentype"
 )
 
+// RenderOp is one draw instruction: draw Text at (X,Y).
 type RenderOp struct {
-	Text	string
-	X, Y	int
+	Text	 string
+	X, Y	 int
 }
 
 type Context struct {
-	Face		font.Face
-	LineHeight	int
-	MaxWidth	int
-	ListIndent	int
+	Faces			map[string]font.Face
+	DefaultFace		font.Face
+	LineHeights		map[string]int
+	DefaultLineHeight	int
+	MaxWidth		int
+	ListIndent		int
 }
 
-// get total content height
+func NewContext(maxWidth int) (*Context, error) {
+	// 1) Parse the raw TTF bytes
+	ttFont, err := opentype.Parse(goregular.TTF)
+	if err != nil {
+		return nil, fmt.Errorf("parsing gofont: %w", err)
+	}
+
+	// 2) Define the size (pt) you want per tag
+	sizes := map[string]float64{
+		"h1": 32,
+		"h2": 24,
+		"h3": 18,
+		"p":  14,
+		"li": 14,
+	}
+
+	faces := make(map[string]font.Face, len(sizes))
+	lineHeights := make(map[string]int, len(sizes))
+
+	// 3) Build a font.Face for each tag via opentype.NewFace
+	for tag, size := range sizes {
+		face, err := opentype.NewFace(ttFont, &opentype.FaceOptions{
+			Size:	size,
+			DPI:	 72,
+			Hinting: font.HintingFull,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("creating face for %s: %w", tag, err)
+		}
+		faces[tag] = face
+		lineHeights[tag] = face.Metrics().Height.Round()
+	}
+
+	defaultFH := lineHeights["p"]
+	return &Context{
+		Faces:			 faces,
+		DefaultFace:	   faces["p"],
+		LineHeights:	   lineHeights,
+		DefaultLineHeight: defaultFH,
+		MaxWidth:		  maxWidth,
+		ListIndent:		20,
+	}, nil
+}
+
 func Render(root *dom.Node, ctx *Context) ([]RenderOp, int) {
 	var ops []RenderOp
 	y := 0
@@ -30,104 +77,113 @@ func Render(root *dom.Node, ctx *Context) ([]RenderOp, int) {
 }
 
 func walk(n *dom.Node, indent int, y *int, ops *[]RenderOp, ctx *Context) {
-	switch n.Type {
-	case dom.ElementNode:
+	if n.Type == dom.ElementNode {
 		tag := strings.ToLower(n.Data)
 		switch tag {
-		case "p", "div", "h1", "h2", "h3":
-			*y += ctx.LineHeight
-			emitText(flattenText(n), indent, y, ops, ctx)
-			*y += ctx.LineHeight
-		case "ul":
-			*y += ctx.LineHeight
-			for _, li := range n.GetElementsByTagName("li") {
-				renderListItem(li, indent, y, ops, ctx, false, 0)
-			}
-			*y += ctx.LineHeight
-		case "ol":
-			*y += ctx.LineHeight
+		case "h1", "h2", "h3", "p":
+			*y += ctx.LineHeights[tag]
+			emitText(tag, flatten(n), indent, y, ops, ctx)
+			*y += ctx.LineHeights[tag]
+
+		case "ul", "ol":
+			*y += ctx.DefaultLineHeight
 			idx := 1
-			for _, li := range n.GetElementsByTagName("li") {
-				renderListItem(li, indent, y, ops, ctx, true, idx)
-				idx++
+			for _, c := range n.Children {
+				if strings.ToLower(c.Data) == "li" {
+					renderListItem(c, indent, y, ops, ctx, tag == "ol", idx)
+					if tag == "ol" {
+						idx++
+					}
+				}
 			}
-			*y += ctx.LineHeight
+			*y += ctx.DefaultLineHeight
+
 		default:
 			for _, c := range n.Children {
 				walk(c, indent, y, ops, ctx)
 			}
 		}
-	case dom.TextNode:
-		emitText(n.Data, indent, y, ops, ctx)
+	} else if n.Type == dom.TextNode {
+		emitText("p", n.Data, indent, y, ops, ctx)
 	}
 }
 
-func flattenText(n *dom.Node) string {
+func renderListItem(n *dom.Node, indentPtr int, y *int, ops *[]RenderOp, ctx *Context, numbered bool, idx int) {
+	prefix := "- "
+	// prefix := "• " // in case we want fancy :shrug:
+	if numbered {
+		prefix = fmt.Sprintf("%d. ", idx)
+	}
+	level := countLists(n)
+	newIndent := indentPtr + ctx.ListIndent*(level-1)
+	emitText("li", prefix+flatten(n), newIndent, y, ops, ctx)
+}
+
+func flatten(n *dom.Node) string {
 	var sb strings.Builder
 	var f func(*dom.Node)
 	f = func(x *dom.Node) {
 		if x.Type == dom.TextNode {
 			sb.WriteString(x.Data)
 		}
-		for _, c := range x.Children { f(c) }
+		for _, c := range x.Children {
+			f(c)
+		}
 	}
 	f(n)
 	return sb.String()
 }
 
-func renderListItem(n *dom.Node, indent int, y *int, ops *[]RenderOp, ctx *Context, numbered bool, idx int) {
-	var prefix string
-	if numbered {
-		prefix = fmt.Sprintf("%d. ", idx)
-	} else {
-		prefix = "- "
-		// prefix = "• " // in case we want fanciness :shrug: 
+func emitText(tag, text string, indent int, y *int, ops *[]RenderOp, ctx *Context) {
+	face, ok := ctx.Faces[tag]
+	if !ok {
+		face = ctx.DefaultFace
 	}
-	level := countAncestorLists(n)
-	newIndent := indent + ctx.ListIndent * (level - 1)
-	text := prefix + flattenText(n)
-	emitText(text, newIndent, y, ops, ctx)
-}
+	lineH, ok := ctx.LineHeights[tag]
+	if !ok {
+		lineH = ctx.DefaultLineHeight
+	}
 
-func emitText(s string, indent int, y *int, ops *[]RenderOp, ctx *Context) {
-	// guard against 0 width
 	if ctx.MaxWidth <= 0 {
-		*ops = append(*ops, RenderOp{Text: s, X: indent, Y: *y})
-		*y += ctx.LineHeight
+		*ops = append(*ops, RenderOp{Text: text, X: indent, Y: *y})
+		*y += lineH
 		return
 	}
 
-	words := strings.Fields(s)
+	drawer := font.Drawer{Face: face}
+	words := strings.Fields(text)
 	if len(words) == 0 {
-		*y += ctx.LineHeight
+		*y += lineH
 		return
 	}
-	drawer := &font.Drawer{Face: ctx.Face}
 	line := ""
 	for _, w := range words {
-		test := w
-		if line != "" { test = line + " " + w }
-		adv := drawer.MeasureString(test).Round()
-		if adv + indent > ctx.MaxWidth && line != "" {
+		candidate := w
+		if line != "" {
+			candidate = line + " " + w
+		}
+		adv := drawer.MeasureString(candidate).Ceil()
+		if adv+indent > ctx.MaxWidth && line != "" {
 			*ops = append(*ops, RenderOp{Text: line, X: indent, Y: *y})
-			*y += ctx.LineHeight
+			*y += lineH
 			line = w
-		} else { line = test }
+		} else {
+			line = candidate
+		}
 	}
-
 	if line != "" {
 		*ops = append(*ops, RenderOp{Text: line, X: indent, Y: *y})
-		*y += ctx.LineHeight
+		*y += lineH
 	}
 }
 
-func countAncestorLists(n *dom.Node) int {
-	cnt := 0
+func countLists(n *dom.Node) int {
+	lvl := 0
 	for p := n.Parent; p != nil; p = p.Parent {
 		t := strings.ToLower(p.Data)
 		if t == "ul" || t == "ol" {
-			cnt++
+			lvl++
 		}
 	}
-	return cnt
+	return lvl
 }
